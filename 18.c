0fe6e4d40cabb8e4a07296de88d6869800c928cd
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-27 12:52:45 -0300

    Update 18.c

--- 18.c
+++ 18.c
@@ -1,3 +1,8 @@
+/*Faça um programa C que leia dez números que representam as notas de dez alunos de
+uma disciplina. As notas variam de zero até dez (0 a 10). O programa deve validar a entrada
+de dados e obter: a soma das notas, a média das notas, a maior nota, a menor nota. Assuma
+que as notas são informadas corretamente no intervalo de 1 a 10.*/
+
 #include <stdio.h>
 #include <string.h>
 #define TamanhoArry 2

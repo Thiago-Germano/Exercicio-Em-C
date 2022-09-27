0e028f3911af01ebb3ae2bb9a70f8637a033be98
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-27 20:38:36 -0300

    Update 23.c

--- 23.c
+++ 23.c
@@ -1,4 +1,8 @@
+/*Escreva um programa que solicite seis números do tipo inteiro ao usuário e os armazene
+em um vetor, depois o programa deverá apresentar na tela os números na ordem inversa do
+qual foram digitados. */
 #include <stdio.h>
+
 int main(){
     int num[6],soma=0,subtr=0;
 

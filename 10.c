d475bd3b6368fcecfcba87ee2e6cd0d5a20719ba
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-27 12:50:27 -0300

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,5 +1,13 @@
+/*Faça um programa que receba um número inteiro e que verifique se esse número é par
+ou ímpar. O programa deve informar se o número é par, caso afirmativo informar também se
+é ou não maior que 15 ou se o número é ímpar, caso afirmativo informar se é ou não menor
+que 50.*/
+
+
+
+
 #include <stdio.h>
-#include	<locale.h>
+#include <locale.h>
 
 int main(){
     setlocale(0, "Portuguese");
@@ -10,16 +18,16 @@ int main(){
     if((num%2) == 0){
         printf("O valor e par. ");
         if(num>=15){
-            printf("Alem disso, o valor � maior que 15");
+            printf("Alem disso, o valor é maior que 15");
         }else{
             printf("Alem disso, o valor e menor que 15");
         }
     }else{
         printf("O valor e impar. ");
         if(num<=50){
-            printf("Alem disso, o valor � maior que 50");
+            printf("Alem disso, o valor é maior que 50");
         }else{
-            printf("Alem disso, o valor � menor que 50");
+            printf("Alem disso, o valor é menor que 50");
         }
     }
 

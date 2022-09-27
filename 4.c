72cd21af945edd5925fa0a4fd6b3b1c4a3c03c3c
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-27 20:41:08 -0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,7 +1,8 @@
 /*Faça um programa que lê dois valores e imprime: (0,1)
 a) se o primeiro valor for menor que o segundo, a lista de valores do primeiro até o segundo;
 b) se o primeiro valor for menor que o segundo a lista de valores do segundo até o primeiro
-em ordem decrescente;*/
+em ordem decrescente;
+c) se ambos forem iguais a mensagem "valores iguais".*/
 #include <stdio.h>
 
 int main()

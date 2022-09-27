e9fcef823491a03dc98ea2997ad07f354795ec9a
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-27 20:44:12 -0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,3 +1,13 @@
+/*Escreva um programa que leia 3 notas de um aluno e a m√©dia (ME) das notas dos
+exerc√≠cios realizados por ele. Calcular a m√©dia de aproveitamento, usando a f√≥rmula: MA =
+(N1 + N2*2 + N3*3 + ME)/7. A partir da m√©dia, informar o conceito de acordo com a tabela:
+(0,1)
+maior ou igual a 9 A
+maior ou igual a 7.5 e menor que 9 B
+maior ou igual a 6 e menor que 7.5 C
+maior ou igual a 4 e menor que 6 D
+menor que 4 E*/
+
 #include <stdio.h>
 #include	<locale.h>
 
@@ -13,27 +23,27 @@ int main(){
     scanf("%f", &nota2);
     printf("Digite sua terceira nota: ");
     scanf("%f", &nota3);
-    printf("Digite sua mÈdia das notas de exercÌcios: ");
+    printf("Digite sua m√©dia das notas de exerc√≠cios: ");
     scanf("%f", &media_exercicios);
 
     media_geral = (nota1 + nota2*2 + nota3*3 + media_exercicios) / 7;
 
     if(media_geral >= 9){
-        printf("Sua mÈdia È %.2f e o seu conceito È A",media_geral);
+        printf("Sua m√©dia √© %.2f e o seu conceito √© A",media_geral);
     }
     else if(media_geral >= 7.5 && media_geral < 9){
-        printf("Sua mÈdia È %.2f e o seu conceito È B",media_geral);
+        printf("Sua m√©dia √© %.2f e o seu conceito √© B",media_geral);
     }
     else if(media_geral >= 6 && media_geral < 7.5){
-        printf("Sua mÈdia È %.2f e o seu conceito È C",media_geral);
+        printf("Sua m√©dia √© %.2f e o seu conceito √© C",media_geral);
     }
     else if(media_geral >= 4 && media_geral < 6){
-        printf("Sua mÈdia È %.2f e o seu conceito È D",media_geral);
+        printf("Sua m√©dia √© %.2f e o seu conceito √© D",media_geral);
     }
     else if(media_geral < 4){
-        printf("Sua mÈdia È %.2f e o seu conceito È E",media_geral);
+        printf("Sua m√©dia √© %.2f e o seu conceito √© E",media_geral);
     }
     else{
-        printf("ERRO!! Sua mÈdia est· maior do que o limite, tente novamente!");
+        printf("ERRO!! Sua m√©dia est√° maior do que o limite, tente novamente!");
     }
 }
